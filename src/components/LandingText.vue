f165586b781088db6e1c5685b5b832ea7503ed1e
MaryahKim <maryapitaz@gmail.com>
2022-03-03 13:47:34 +0300

    Create LandingText.vue
    create the H1 component

---
+++ src/components/LandingText.vue
@@ -0,0 +1,15 @@
+<template>
+  <p> Hello Fellow Shoppers! We're currrently building oour new fashion store.
+      Add your email below to stay up-to-date with announcements and our launch deals.
+  </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
