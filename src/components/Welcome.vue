85fb9dfd1fe514f76ec07716c11298ef90c4fa8c
MaryahKim <maryapitaz@gmail.com>
2022-03-03 13:48:05 +0300

    Create Welcome.vue
    create header component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>
+        <span>WE'RE </span> 
+        <br>
+        <span>COMING <br> SOON</span> 
+</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
