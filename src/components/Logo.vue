f2ef5e8729394fa499294abc5b4eac1b527ea923
MaryahKim <maryapitaz@gmail.com>
2022-03-03 13:47:49 +0300

    Create Logo.vue
    add a logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <img :src="logo">
+  </div>
+</template>
+
+<script>
+import logo from  '../assets/images/logo.svg'
+
+export default {
+name: 'Logo',
+props: '',
+
+data: function() {
+    return{
+        logo,
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
