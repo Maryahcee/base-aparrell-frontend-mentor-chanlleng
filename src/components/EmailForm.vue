fd4f21b3996c0db2467cf69e3a7e2357816380a3
MaryahKim <maryapitaz@gmail.com>
2022-03-04 19:00:34 +0300

    Create EmailForm.vue
    Create EmailForm component

---
+++ src/components/EmailForm.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="email">
+  <input v-model="address" placeholder="Email Adress">
+  <button class="click">
+          <img :src="icon">
+      </button>
+</div>
+</template>
+
+<script>
+import icon from  '../assets/images/icon-arrow.svg'
+ 
+export default {
+    name: 'Form',
+    name: 'icon',
+
+    props: ({ 
+        Form:'',
+        icon: '',
+    }),
+
+  data: function() {
+    return{
+      address:'',
+      icon:'',
+    }
+  }
+}
+</script>
+
+<style>
+input {
+    padding: 13px;
+    height: 28px;
+    border-radius: 15px;
+    border-style: none;
+}
+.click {
+    background-color:hsl(0, 36%, 70%);
+    padding: 3px;
+    margin: -19px;
+    border-radius: 30px;
+    width: 50px;
+    height: 28px;
+    border-style: none;
+}
+</style>
