d885f9a3bf9891adb7a36aa1626ed97e4d8a7927
MaryahKim <maryapitaz@gmail.com>
2022-03-07 17:22:42 +0300

    Update Welcome.vue
    import google fonts
    styled header, pink-text, and fonts

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,9 +1,11 @@
 <template>
-  <h1>
-        <span>WE'RE </span> 
+<div class="header">
+    <h1 class="header-font">
+        <span class="pink-text">WE'RE </span> 
         <br>
         <span>COMING <br> SOON</span> 
 </h1>
+</div>
 </template>
 
 <script>
@@ -13,5 +15,21 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap');
 
+.header{
+  padding: 2rem 0;
+}
+
+.pink-text {
+color: hsl(0, 36%, 70%);
+font-weight: 300;
+}
+
+.header-font {
+  font-family: 'Josefin Sans';
+  font-weight: 600;
+  line-height: 1.3;
+  letter-spacing: 5px;
+}
 </style>
